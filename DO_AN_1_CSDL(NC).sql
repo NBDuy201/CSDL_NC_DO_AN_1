f4c0d407c9c676d8d210205928e675364f0f3c98
PhamAnhTuan084 <tuansoi19127084@gmail.com>
2021-10-20 19:19:51 +0700

    ĐỔI TÊN - VÙNG GIÁ TRỊ

--- DO_AN_1_CSDL(NC).sql
+++ DO_AN_1_CSDL(NC).sql
@@ -1,53 +1,57 @@
-﻿USE DoAn1_CSDL_NC
+﻿CREATE DATABASE DoAn1_CSDL_NC
+GO
+USE DoAn1_CSDL_NC
 GO
 
-CREATE TABLE [KhachHang] (
-  [MakH] Varchar(4),
-  [Ho] Varchar(10),
-  [Ten] Varchar(30),
-  [Ngsinh] Datetime,
-  [SoNha] Varchar(4),
-  [Duong] Varchar(20),
-  [Phuong] Varchar(20),
-  [Quan] Varchar(20),
-  [Tpho] Varchar(30),
-  [DienThoai] Varchar(10),
-  PRIMARY KEY ([MakH])
-);
+CREATE TABLE KhachHang
+(
+	MakH Varchar(8),
+	Ho Varchar(10),
+	Ten Varchar(30),
+	Ngsinh Datetime,
+	SoNha Varchar(4),
+	Duong Varchar(50),
+	Phuong Varchar(50),
+	Quan Varchar(50),
+	Tpho Varchar(50),
+	DienThoai Varchar(15),
+	PRIMARY KEY (MakH)
+)
+GO
+CREATE TABLE HoaDon
+(
+	MaHD Varchar(8),
+	MaKH Varchar(8),
+	NgayLap Datetime,
+	TongTien int,
+	PRIMARY KEY (MaHD),
+	CONSTRAINT FK_HD_KH_MaKH FOREIGN KEY (MaKH) REFERENCES KhachHang(MakH)
+)
+GO
+CREATE TABLE SanPham
+(
+	MaSP Varchar(8),
+	TenSP Varchar(30),
+	SoLuongTon int,
+	Mota Varchar(100),
+	Gia int,
+	PRIMARY KEY (MaSP)
+)
+GO
+CREATE TABLE CT_HoaDon 
+(
+	MaHD Varchar(8),
+	MaSP Varchar(8),
+	SoLuong int,
+	GiaBan int,
+	GiaGiam int,
+	ThanhTien int,
+	PRIMARY KEY (MaHD, MaSP),
+	CONSTRAINT FK_CtHD_HD_MaHD FOREIGN KEY (MaHD) REFERENCES HoaDon(MaHD),
+	CONSTRAINT FK_CtHD_SP_MaSP FOREIGN KEY (MaSP) REFERENCES SanPham(MaSP)
+)
 GO
 
-CREATE TABLE [HoaDon] (
-  [MaHD] Varchar(4),
-  [MaKH] Varchar(4),
-  [NgayLap] Datetime,
-  [TongTien] int,
-  PRIMARY KEY ([MaHD]),
-  CONSTRAINT [FK_HD_KH_MaKH] FOREIGN KEY ([MaKH]) REFERENCES [KhachHang]([MakH])
-);
-GO
-
-CREATE TABLE [SanPham] (
-  [MaSP] Varchar(4),
-  [TenSP] Varchar(20),
-  [SoLuongTon] int,
-  [Mota] Varchar(40),
-  [Gia] int,
-  PRIMARY KEY ([MaSP])
-);
-GO
-
-CREATE TABLE [CT_HoaDon] (
-  [MaHD] Varchar(4),
-  [MaSP] Varchar(4),
-  [SoLuong] int,
-  [GiaBan] int,
-  [GiaGiam] int,
-  [ThanhTien] int,
-  PRIMARY KEY ([MaHD], [MaSP]),
-  CONSTRAINT [FK_CtHD_HD_MaHD] FOREIGN KEY ([MaHD]) REFERENCES [HoaDon]([MaHD]),
-  CONSTRAINT [FK_CtHD_SP_MaSP] FOREIGN KEY ([MaSP]) REFERENCES [SanPham]([MaSP])
-);
-GO
 
 --ALTER TABLE [HoaDon]
 --ADD TongTien AS 
